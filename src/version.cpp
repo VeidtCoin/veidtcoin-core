840441d98e14e4522c3b8bb1bbd94a53dc1aff50
mylynxcc <payments@mylynx.cc>
2025-07-09 00:23:53 +0100

    Create version.cpp

---
+++ src/version.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+const char* GENESIS_QUOTE = R"(
+“The old currencies were bound to names, nations, and noise.  
+This one is born in silence, unseen — a whisper of wealth beneath the system.”  
+— Taro Veidt, First Cipherholder
+)";
+
+void print_genesis_quote() {
+    std::cout << GENESIS_QUOTE << std::endl;
+}

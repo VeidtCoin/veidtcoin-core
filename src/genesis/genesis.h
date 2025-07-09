40886575449660cd0cc0fbdea30c2dbe576a031e
mylynxcc <payments@mylynx.cc>
2025-07-09 18:44:24 -0400

    Create genesis.h

---
+++ src/genesis/genesis.h
@@ -0,0 +1,12 @@
+#ifndef VEIDTCOIN_GENESIS_H
+#define VEIDTCOIN_GENESIS_H
+
+#include <string>
+
+namespace Genesis {
+    std::string getGenesisQuote();
+    std::string getGenesisHashSeed();
+    std::string getTimestampNote();
+}
+
+#endif

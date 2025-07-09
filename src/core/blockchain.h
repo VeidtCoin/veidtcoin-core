201f82de42e019a23418e9431308164b6700ef01
mylynxcc <payments@mylynx.cc>
2025-07-09 17:12:12 -0400

    Create blockchain.h

---
+++ src/core/blockchain.h
@@ -0,0 +1,31 @@
+#ifndef VEIDTCOIN_BLOCKCHAIN_H
+#define VEIDTCOIN_BLOCKCHAIN_H
+
+#include <vector>
+#include <string>
+#include <cstdint>
+
+struct Block {
+    uint64_t index;
+    uint64_t timestamp;
+    std::string prevHash;
+    std::string hash;
+    std::string merkleRoot;
+    std::string extraData;
+};
+
+class Blockchain {
+public:
+    Blockchain();
+    const Block& getLatestBlock() const;
+    void addBlock(const std::string& data);
+    bool validateChain() const;
+
+private:
+    std::vector<Block> chain;
+    Block createGenesisBlock();
+    Block createBlock(const std::string& data);
+    std::string calculateHash(const Block& block) const;
+};
+
+#endif

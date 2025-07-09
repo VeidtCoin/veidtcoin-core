b254a592371e74e769ec9bc49d5b227a49a2d426
mylynxcc <payments@mylynx.cc>
2025-07-09 18:37:53 -0400

    Create net_config.h

---
+++ src/network/net_config.h
@@ -0,0 +1,11 @@
+#ifndef VEIDTCOIN_NET_CONFIG_H
+#define VEIDTCOIN_NET_CONFIG_H
+
+namespace NetConfig {
+    constexpr int DEFAULT_PORT = 19191;
+    constexpr int MAX_CONNECTIONS = 8;
+    constexpr const char* DEFAULT_IP = "127.0.0.1";
+    constexpr const char* TESTNET_NAME = "ShadowNet";
+}
+
+#endif

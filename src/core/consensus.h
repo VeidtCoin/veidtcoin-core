27fa06c1d3f333fb433ee8484d75787b694fa251
mylynxcc <payments@mylynx.cc>
2025-07-09 17:13:32 -0400

    Create consensus.h

---
+++ src/core/consensus.h
@@ -0,0 +1,10 @@
+#ifndef VEIDTCOIN_CONSENSUS_H
+#define VEIDTCOIN_CONSENSUS_H
+
+namespace Consensus {
+    constexpr uint64_t BLOCK_TIME_SECONDS = 45;
+    constexpr uint64_t MAX_SUPPLY = 21000000;
+    constexpr uint64_t INITIAL_BLOCK_REWARD = 10;
+}
+
+#endif
